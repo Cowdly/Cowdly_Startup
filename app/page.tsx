92f87a8f5e183e4a56aa5ff094087efb3f6c795b
Ahmed-mohamed11 <ahmedmohamed0aa349@gmail.com>
2024-08-15 00:15:24 +0300

    updates

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+ import React from "react";
+ import Project from '../components/project'
+export default function Home() {
+  return <main className="overflow-hidden"><Project /></main>;
+}
---
+++ components/project.jsx
@@ -0,0 +1,21 @@
+import style from "./project.module.css";
+ import Slider from "./sliderProject";
+
+const Project = () => {
+ 
+  return (
+    <div
+      className={`mx-auto ${style.content}`}
+      id="projects"
+    >
+      <div className="position-relative  z-50 d-flex mb-3 mt-3 align-items-center justify-content-center">
+        <h1 className="  px-3 mb-3 text-3xl   text-[blue]">
+           projects 
+        </h1>
+      <Slider />
+      </div>
+    </div>
+  );
+};
+
+export default Project;
