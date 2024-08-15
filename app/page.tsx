7696aef6332e51b996f7910adf7b4813763aaf7b
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2024-08-15 15:45:16 +0300

    ContactUs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Project from "../components/project";
+import ContactUs from "../components/contactUs";
 export default function Home() {
   return (
     <main className="overflow-hidden">
