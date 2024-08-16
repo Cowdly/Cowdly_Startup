b871f6015436a376362d2915ae1bae9234fbb75f
Ahmed-mohamed11 <ahmedmohamed0aa349@gmail.com>
2024-08-17 01:28:32 +0300

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import Project from "../components/project/project";
 import Content from "../components/institution/Content";
+import Team from "../components/team/team";
    export default function Home() {
   return (
     <main className="overflow-hidden">
       <Content />
       <Project />
+      <Team/>
     </main>
   );
 }
